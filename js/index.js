af7093296ceb6067de847118172a8cc5ee6961d3
Facundo-Carranza <facundocarranza6@gmail.com>
2024-07-03 19:43:17 -0300

    - Modificado el diseño en css/app.css   - Corregido error tipográfico en la clase .hero   - Ajustes en el estilo del botón en :hover - Empecé a escribir código de JavaScript   - Implementada función nextSlide para avanzar a la siguiente diapositiva   - Implementada función prevSlide para retroceder a la diapositiva anterior   - Implementada función hideAllSlide para ocultar todas las diapositivas

---
+++ js/index.js
@@ -0,0 +1,30 @@
+let slide_index = 0;
+let slide_play = true;
+let slides = document.querySelectorAll(".slide");
+
+hideAllSlide = () => {
+  slides.forEach((e) => {
+    e.classList.remove("active");
+  });
+};
+
+showSlide = () => {
+  hideAllSlide();
+  slides[slide_index].classList.add("active");
+};
+
+nextSlide = () => {
+  slide_index = slide_index + 1 >= slides.length ? 0 : slide_index + 1;
+};
+
+prevSlide = () => {
+  slide_index = slide_index - 1 < 0 ? slides.length - 1 : slide_index - 1;
+};
+
+showSlide()
+
+setInterval(() => {
+    if (!slide_play)return
+    nwxtSlide()
+    showSlide()
+},1000);
